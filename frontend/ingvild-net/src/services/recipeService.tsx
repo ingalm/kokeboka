0d9d648799439302e382c4d29bb24cc572afb1d8
ingval <ingvild.almasbakk@gmail.com>
2024-01-18 12:55:29 +0100

    Minor deletion

--- frontend/ingvild-net/src/services/recipeService.tsx
+++ frontend/ingvild-net/src/services/recipeService.tsx
@@ -1,37 +1,6 @@
 import { AxiosResponse } from 'axios';
 import api from './api';
-
-export interface Recipe {
-    id: number;
-    slug: string;
-    recipe_name: string;
-    last_edited: string;
-    image_url: string;
-    ingredients: Ingredient[];
-    steps: Step[];
-}
-
-export interface RecipeData {
-    recipe_name: string;
-    last_edited: string;
-    image_url: string;
-}
-
-export interface Ingredient {
-    id: number;
-    recipe_id: string;
-    ingredient_name: string;
-    amount: number;
-    slug: string;
-    measurement_type: string;
-}
-
-export interface Step {
-    id: number
-    recipe_id: string;
-    slug: string;
-    info: string;
-}
+import { Recipe, Ingredient, Step, RecipeData } from './types';
 
 const CreateRecipe = (recipeData: RecipeData, ingredients: Ingredient[], steps: Step[]): 
     Promise<{ success: boolean; recipeId: any; } | { success: boolean; error: any; }> => {
